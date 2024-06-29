2c7e8f524695d7cf4b986b22487f5d18ec102975
marcocarneiro <marco.carneiro7@gmail.com>
2024-06-29 16:01:36 -0300

    Execução da animação Lottie no componente

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -3,7 +3,9 @@ import { Box, IconButton, TextField, Typography, Autocomplete } from '@mui/mater
 import { CatchingPokemon } from '@mui/icons-material';
 import debounce from 'lodash.debounce';
 import { styled } from '@mui/system';
-import backgroundImage from '../assets/background.jpg'; 
+import Lottie from 'react-lottie';
+import backgroundImage from '../assets/background.jpg';
+import animationData from '../assets/explode.json';
 
 interface Pokemon {
     name: string;
@@ -48,6 +50,7 @@ const Quiz: React.FC = () => {
     const [pokemonNames, setPokemonNames] = useState<string[]>([]);
     const [isFocused, setIsFocused] = useState<boolean>(false);
     const [hasAnswered, setHasAnswered] = useState<boolean>(false);
+    const [showAnimation, setShowAnimation] = useState<boolean>(false);
 
     useEffect(() => {
         getPokemon();
@@ -65,6 +68,7 @@ const Quiz: React.FC = () => {
     };
 
     const getPokemon = async () => {
+        setShowAnimation(true);
         const randomId = Math.floor(Math.random() * 150) + 1;
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
         const data: Pokemon = await response.json();
@@ -108,63 +112,89 @@ const Quiz: React.FC = () => {
 
     if (!currentPokemon) return null; // Ou algum indicador de carregamento
 
+    const defaultOptions = {
+        loop: false,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+        }
+    };
+
     return (
         <BackgroundBox>
             <Typography variant="h2" sx={{ position: 'relative', width: '360px'}}>
                 <img src="/logo-alltype.svg" alt="Pokémon Logo" style={{ width: '250px' }} /> 
                 <Typography variant="h3" sx={{ position: 'absolute', top: '30px', right: '0'}}>Quiz</Typography>
             </Typography>
-            <img className="pokemon-image" src={currentPokemon.sprites?.other['official-artwork'].front_default || currentPokemon.sprites.front_default} 
-            alt="Pokémon Image" style={{ width: '200px', margin: '20px 0' }} />
-            <Typography variant="body1" id="question" sx={{ marginBottom: '16px'}}>Qual é o nome deste Pokémon?</Typography>
-            <Autocomplete
-                freeSolo
-                options={pokemonNames}
-                inputValue={answer}
-                onInputChange={handleInputChange}
-                sx={{ width: '300px', textAlign: 'center' }}
-                renderInput={(params) => (
-                    <TextField
-                        {...params}
-                        id="answer"
-                        label="Digite sua resposta"
-                        variant="standard"
-                        InputLabelProps={{
-                            style: { textAlign: 'center', width: '100%' },
-                            shrink: true,
-                        }}
-                        onFocus={() => setIsFocused(true)}
-                        onBlur={() => setIsFocused(false)}
-                        sx={{
-                            '& .MuiInputLabel-root': {
-                                left: '50%',
-                                top: '18px',
-                                transform: 'translateX(-50%)',
-                                display: isFocused || hasAnswered ? 'none' : 'block',
-                            },
-                            '& .MuiInputBase-input': {
-                                textAlign: 'center', // Centraliza o texto do input
-                            },
-                        }}
+            {showAnimation && (
+                <Lottie
+                    options={defaultOptions}
+                    height={200}
+                    width={200}
+                    eventListeners={[
+                        {
+                            eventName: 'complete',
+                            callback: () => setShowAnimation(false),
+                        },
+                    ]}
+                />
+            )}
+            {!showAnimation && (
+                <>
+                    <img className="pokemon-image" src={currentPokemon.sprites?.other['official-artwork'].front_default || currentPokemon.sprites.front_default} 
+                    alt="Pokémon Image" style={{ width: '200px', margin: '20px 0' }} />
+                    <Typography variant="body1" id="question" sx={{ marginBottom: '16px'}}>Qual é o nome deste Pokémon?</Typography>
+                    <Autocomplete
+                        freeSolo
+                        options={pokemonNames}
+                        inputValue={answer}
+                        onInputChange={handleInputChange}
+                        sx={{ width: '300px', textAlign: 'center' }}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                id="answer"
+                                label="Digite sua resposta"
+                                variant="standard"
+                                InputLabelProps={{
+                                    style: { textAlign: 'center', width: '100%' },
+                                    shrink: true,
+                                }}
+                                onFocus={() => setIsFocused(true)}
+                                onBlur={() => setIsFocused(false)}
+                                sx={{
+                                    '& .MuiInputLabel-root': {
+                                        left: '50%',
+                                        top: '18px',
+                                        transform: 'translateX(-50%)',
+                                        display: isFocused || hasAnswered ? 'none' : 'block',
+                                    },
+                                    '& .MuiInputBase-input': {
+                                        textAlign: 'center', // Centraliza o texto do input
+                                    },
+                                }}
+                            />
+                        )}
                     />
-                )}
-            />
-            <Box sx={{ marginTop: '40px', textAlign: 'center', display: 'flex', alignItems: 'center' }}>
-                <IconButton                      
-                    onClick={checkAnswer} 
-                    sx={{ 
-                        display: 'flex', 
-                        flexDirection: 'column', 
-                        alignItems: 'center',
-                        '&:focus': { outline: 'none' }, // Remove a borda ao clicar
-                    }}
-                    disableRipple // Desativa o ripple effect
-                >
-                    <CatchingPokemon />
-                    <Typography variant="button">Enviar</Typography>
-                </IconButton>
-            </Box>                
-            <Typography variant="body1" id="feedback">{feedback}</Typography>
+                    <Box sx={{ marginTop: '40px', textAlign: 'center', display: 'flex', alignItems: 'center' }}>
+                        <IconButton                      
+                            onClick={checkAnswer} 
+                            sx={{ 
+                                display: 'flex', 
+                                flexDirection: 'column', 
+                                alignItems: 'center',
+                                '&:focus': { outline: 'none' }, // Remove a borda ao clicar
+                            }}
+                            disableRipple // Desativa o ripple effect
+                        >
+                            <CatchingPokemon />
+                            <Typography variant="button">Enviar</Typography>
+                        </IconButton>
+                    </Box>                
+                    <Typography variant="body1" id="feedback">{feedback}</Typography>
+                </>
+            )}
         </BackgroundBox>
     );
 };
