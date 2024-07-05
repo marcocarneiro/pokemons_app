16c792d4a8e928cb479d5f0fea17e95c51e2ebcc
marcocarneiro <marco.carneiro7@gmail.com>
2024-07-05 09:39:51 -0300

    Ajuste na aparência do componente Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,13 +7,15 @@ const Header: React.FC = () => {
     return (
         <AppBar position="static" sx={{ backgroundColor: '#ed5564' }}>
             <Toolbar>
-                <Box sx={{ flexGrow: 1 }}>
+                <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}>
                     <Link to="/">
                         <img src="/logo-alltype.svg" alt="Pokémon Logo" style={{ width: '120px' }} />
                     </Link>
+                    <Box sx={{ flexGrow: 1, mx: 2 }}>
+                        <SearchBar />
+                    </Box>
                 </Box>
                 <Box sx={{ display: 'flex', gap: 2 }}>
-                    <SearchBar />
                     <Button color="inherit" component={Link} to="/">
                         Tabela de Pokemons
                     </Button>
