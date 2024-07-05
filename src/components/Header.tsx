66406c14a3744b8c1086d7c14769ea02c5d34bd1
marcocarneiro <marco.carneiro7@gmail.com>
2024-07-05 09:51:11 -0300

    Renderização condicional de SearchBar dentro de Header - exibir somente na Home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { AppBar, Box, Toolbar, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import SearchBar from './SearchBar';
 
 const Header: React.FC = () => {
+    const location = useLocation();
+    const isHomePage = location.pathname === '/';
+
     return (
         <AppBar position="static" sx={{ backgroundColor: '#ed5564' }}>
             <Toolbar>
-                <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}>
+                <Box sx={{ flexGrow: 1 }}>
                     <Link to="/">
                         <img src="/logo-alltype.svg" alt="Pokémon Logo" style={{ width: '120px' }} />
                     </Link>
-                    <Box sx={{ flexGrow: 1, mx: 2 }}>
+                </Box>
+                {isHomePage && (
+                    <Box sx={{ flexGrow: 1 }}>
                         <SearchBar />
                     </Box>
-                </Box>
+                )}
                 <Box sx={{ display: 'flex', gap: 2 }}>
                     <Button color="inherit" component={Link} to="/">
                         Tabela de Pokemons
